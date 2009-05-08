e24fe6f1445205c9dfba1289474fdd76bd4cf2ab
bschmaus@marvin <bschmaus@marvin>
2009-05-08 09:38:00 -0400

    Moves some things around and stubs out static index page

--- src/webapp/controller/overhead.py
+++ src/webapp/controller/overhead.py
@@ -3,21 +3,24 @@ from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext.webapp import template
 
-class MainPage(webapp.RequestHandler):
+class OverheadPage(webapp.RequestHandler):
   def get(self):
     page = self.request.get('p')
-    self.response.headers['Content-Type'] = 'text/html'
+    
     tmpl_vars = {"page": page}
-    path = os.path.join(os.path.dirname(__file__), 'tmpl/index.tmpl')
+    
+    path = os.path.join(os.path.dirname(__file__), '../tmpl/overhead.tmpl')
+    
+    self.response.headers['Content-Type'] = 'text/html'
     self.response.out.write(template.render(path, tmpl_vars))
 
 application = webapp.WSGIApplication(
-    [('/', MainPage)],
+    [ ('/overhead', OverheadPage) ],
     debug=True
 )
 
 def main():
-  run_wsgi_app(application)
+    run_wsgi_app(application)
 
 if __name__ == "__main__":
-  main()
+    main()
