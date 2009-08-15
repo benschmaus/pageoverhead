90f49ca8717f92e3173ed33242550135444de9af
bschmaus <benjamin.schmaus@gmail.com>
2009-08-15 16:23:40 -0400

    Not tracking eclipse files.  Fixes indent in overhead controller.

--- src/webapp/controller/overhead.py
+++ src/webapp/controller/overhead.py
@@ -6,11 +6,11 @@ from google.appengine.ext.webapp import template
 class OverheadPage(webapp.RequestHandler):
   def get(self):
     page = self.request.get('p')
-    
+
     tmpl_vars = {"page": page}
-    
+
     path = os.path.join(os.path.dirname(__file__), '../tmpl/overhead.tmpl')
-    
+
     self.response.headers['Content-Type'] = 'text/html'
     self.response.out.write(template.render(path, tmpl_vars))
 
